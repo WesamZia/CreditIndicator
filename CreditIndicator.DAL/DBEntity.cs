12c41742cad65a8f6c806ddd728c1a499f2809b3
wesam ziadat <wesam.ziadat@yahoo.com>
2019-11-05 18:02:58 +0200

    initializing DBcontext and seed

---
+++ CreditIndicator.DAL/DBEntity.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.Entity;
+using CreditIndicator.Contracts.Models;
+
+namespace CreditIndicator.DAL
+{
+    public class DBEntity : DbContext
+    {
+        public DBEntity() : base(BuildConnectionString())
+        {
+            Configuration.LazyLoadingEnabled = false;
+            Database.SetInitializer(new IndicatorDataInitializer());
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //modelBuilder.Configurations.Add(new DigitMapper());
+            //modelBuilder.Configurations.Add(new FeatureMapper());
+        }
+
+
+        public virtual DbSet<FeatureModel> Features { get; set; }
+        public virtual DbSet<DigitModel> Digits { get; set; }
+
+
+        private static string BuildConnectionString()
+        {
+            var _conbuilder = new System.Data.SqlClient.SqlConnectionStringBuilder();
+            _conbuilder.IntegratedSecurity = true;
+            _conbuilder.DataSource = @"(LocalDb)\MSSQLLocalDB";
+            _conbuilder.InitialCatalog = "CreditDB";
+            return _conbuilder.ConnectionString;
+        }
+    }
+}
---
+++ CreditIndicator.DAL/IndicatorDataInitializer.cs
@@ -0,0 +1,32 @@
+﻿using CreditIndicator.Contracts.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CreditIndicator.DAL
+{
+    public class IndicatorDataInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<DBEntity>
+    {
+        protected override void Seed(DBEntity _db)
+        {
+            var Properties = new List<FeatureModel>
+            {
+            new FeatureModel{ AcquireDate=20190101,AssetNumber="A00SS1-R",InInventory=true}
+            };
+
+            Properties.ForEach(o => _db.Features.Add(o));
+            _db.SaveChanges();
+
+            var Digits = new List<DigitModel>
+            {
+            new DigitModel{ AcquireDate=20190101,ItemCode=350 ,AssetNumber="B00SS1-R",ItemValue=102.0m,IsFinancial=true}
+            };
+
+            Digits.ForEach(o => _db.Digits.Add(o));
+            _db.SaveChanges();
+
+
+            base.Seed(_db);
+        }
+    }
+}
